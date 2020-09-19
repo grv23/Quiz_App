a406f767b1a81b5212e5facafea1a3e1e1294635
Gaurav Mishra <geeonline20@gmail.com>
2020-09-19 09:31:11 +0530

    Updates

---
+++ precache-manifest.125874d1829f60ac4711c9446e48e893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8061e7b9af5f50a185ba51b34146c8c1",
+    "url": "/Quiz_App/index.html"
+  },
+  {
+    "revision": "211337c2b90a03a4f159",
+    "url": "/Quiz_App/static/css/main.cf7f2383.chunk.css"
+  },
+  {
+    "revision": "4204c63f6d022e8d7931",
+    "url": "/Quiz_App/static/js/2.68abdecb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz_App/static/js/2.68abdecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211337c2b90a03a4f159",
+    "url": "/Quiz_App/static/js/main.b11dc5ec.chunk.js"
+  },
+  {
+    "revision": "21d78e5ef332223c694d",
+    "url": "/Quiz_App/static/js/runtime-main.a2daaafd.js"
+  }
+]);
